a6584b340ae1e1d58b337f6393ac323369848721
Romeo Enso <romeo.enso@connexionltd.com>
2024-10-04 09:33:57 +1000

    Setting testing library

---
+++ test/components/Greet.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import Greet from "../../src/components/Greet";
+
+describe("Greet", () => {
+  it("should render Hello with the name when name is provided", () => {
+    render(<Greet name="Romeo" />);
+    screen.debug();
+    const h1 = screen.getByRole("heading");
+    expect(h1).toBeInTheDocument();
+    expect(h1).toHaveTextContent(/hello romeo/i);
+  });
+  it("should render Login button when name is not provided", () => {
+    render(<Greet />);
+    screen.debug();
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent(/login/i);
+  });
+});
