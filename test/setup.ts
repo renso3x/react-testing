29c87e4bd35251e063efee2fc1e5d1069c8fc2c1
Romeo Enso <romeo.enso@connexionltd.com>
2024-10-08 09:35:20 +1100

    setup msw for mock http

--- test/setup.ts
+++ test/setup.ts
@@ -1,5 +1,9 @@
 import "@testing-library/jest-dom/vitest";
 import ResizeObserver from "resize-observer-polyfill";
+import { server } from "./mocks/server";
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
 
 global.ResizeObserver = ResizeObserver;
 
