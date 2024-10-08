0ad16d2388338b5ae8955717cc1135aaf76bba3a
Romeo Enso <romeo.enso@connexionltd.com>
2024-10-09 10:57:49 +1100

    testing loading and error state

--- test/components/ProductDetail.test.tsx
+++ test/components/ProductDetail.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import ProductDetail from "../../src/components/ProductDetail";
 import { server } from "../mocks/server";
-import { http, HttpResponse } from "msw";
+import { delay, http, HttpResponse } from "msw";
 import { products } from "../mocks/data";
 import { db } from "../mocks/db";
 
@@ -41,5 +41,29 @@ describe("ProductDetail", () => {
 
     const message = await screen.findByText(/Invalid product/i);
     expect(message).toBeInTheDocument();
+  });
+
+  it("should render error if data fetching fails", async () => {
+    server.use(
+      http.get("/products/1", () => {
+        HttpResponse.error();
+      })
+    );
+    render(<ProductDetail productId={0} />);
+
+    expect(await screen.findByText(/error/i)).toBeInTheDocument();
+  });
+
+  it("should render a loading indicator when fetching data", async () => {
+    server.use(
+      http.get("/products/1", async () => {
+        await delay(200);
+        return HttpResponse.json([]);
+      })
+    );
+
+    render(<ProductDetail productId={1} />);
+
+    expect(await screen.findByText(/loading/i)).toBeInTheDocument();
   });
 });
--- test/components/ProductList.test.tsx
+++ test/components/ProductList.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import ProductList from "../../src/components/ProductList";
 import { server } from "../mocks/server";
-import { http, HttpResponse } from "msw";
+import { delay, http, HttpResponse } from "msw";
 import { db } from "../mocks/db";
 
 describe("ProductList", () => {
@@ -34,5 +34,26 @@ describe("ProductList", () => {
 
     const message = await screen.findByText(/no product/i);
     expect(message).toBeInTheDocument();
+  });
+
+  it("should render an error message when there is an error", async () => {
+    server.use(http.get("/products", () => HttpResponse.error()));
+
+    render(<ProductList />);
+
+    expect(await screen.findByText(/error/i)).toBeInTheDocument();
+  });
+
+  it("should render a loading indicator when fetching data", async () => {
+    server.use(
+      http.get("/products", async () => {
+        await delay(200);
+        return HttpResponse.json([]);
+      })
+    );
+
+    render(<ProductList />);
+
+    expect(await screen.findByText(/loading/i)).toBeInTheDocument();
   });
 });
