4602d115a8da49c978ee97ecfdd143589393a2e4
Romeo Enso <romeo.enso@connexionltd.com>
2024-10-21 10:06:49 +1100

    product form testing

--- test/components/ProductForm.test.tsx
+++ test/components/ProductForm.test.tsx
@@ -5,17 +5,63 @@ import {
 } from "@testing-library/react";
 import ProductForm from "../../src/components/ProductForm";
 import { AllProviders } from "../AllProvider";
+import { db } from "../mocks/db";
+import { Category, Product } from "../../src/entities";
 
 describe("ProductForm", () => {
-  it("should render form fields", async () => {
-    render(<ProductForm onSubmit={vi.fn()} />, { wrapper: AllProviders });
+  let category: Category;
+  beforeAll(() => {
+    category = db.category.create();
+  });
 
+  afterAll(() => {
+    db.category.delete({ where: { id: { equals: category.id } } });
+  });
+
+  const renderComponent = (product?: Product) => {
+    render(<ProductForm product={product} onSubmit={vi.fn()} />, {
+      wrapper: AllProviders,
+    });
+
+    return {
+      waitForFormToLoad: () => screen.findByRole("form"),
+      getInputs: () => {
+        return {
+          nameInput: screen.getByPlaceholderText(/name/i),
+          priceInput: screen.getByPlaceholderText(/price/i),
+          categoryInput: screen.getByRole("combobox"),
+        };
+      },
+    };
+  };
+
+  it("should render form fields", async () => {
+    const { waitForFormToLoad, getInputs } = renderComponent();
     // Option: to test a loading state in your screen
     // 1.
-    await screen.findByRole("form");
+    await waitForFormToLoad();
     // 2. await waitForElementToBeRemoved(() => screen.queryByText(/loading/i));
 
-    expect(screen.getByPlaceholderText(/name/i)).toBeInTheDocument();
-    expect(screen.getByPlaceholderText(/price/i)).toBeInTheDocument();
+    const { nameInput, priceInput, categoryInput } = getInputs();
+    expect(nameInput).toBeInTheDocument();
+    expect(priceInput).toBeInTheDocument();
+    expect(categoryInput).toBeInTheDocument();
+  });
+
+  it("should popoulate form fields when editing a product", async () => {
+    const product: Product = {
+      id: 1,
+      name: "Bread",
+      price: 10,
+      categoryId: category.id,
+    };
+    const { waitForFormToLoad, getInputs } = renderComponent(product);
+
+    await waitForFormToLoad();
+    const { nameInput, priceInput, categoryInput } = getInputs();
+
+    expect(nameInput).toHaveValue(product.name);
+    expect(priceInput).toHaveValue(product.price.toString());
+    expect(categoryInput).toHaveTextContent(category.name);
   });
 });
