a55a00775db8fbd5382a40bfe775450381ab47fe
Romeo Enso <romeo.enso@connexionltd.com>
2024-10-25 12:12:00 +1100

    testing protected routes, testing product details page

--- test/Router.test.tsx
+++ test/Router.test.tsx
@@ -1,5 +1,6 @@
 import { screen } from "@testing-library/react";
 import { navigateTo } from "./utils";
+import { db } from "./mocks/db";
 
 describe("Router", () => {
   it("should render the home page for /", () => {
@@ -14,5 +15,28 @@ describe("Router", () => {
     expect(
       screen.getByRole("heading", { name: /products/i })
     ).toBeInTheDocument();
+  });
+
+  it("should render the products details page for /products/:id", async () => {
+    const product = db.product.create();
+    navigateTo("/products/" + product.id);
+
+    expect(
+      await screen.findByRole("heading", { name: product.name })
+    ).toBeInTheDocument();
+
+    db.product.delete({ where: { id: { equals: product.id } } });
+  });
+
+  it("should render the not found page for invalid routes", async () => {
+    navigateTo("/invalid-route/");
+
+    expect(screen.getByText(/not found/i)).toBeInTheDocument();
+  });
+
+  it("should render the admin home page for /admin", async () => {
+    navigateTo("/admin/");
+
+    expect(screen.getByText(/Admin Area/i)).toBeInTheDocument();
   });
 });
